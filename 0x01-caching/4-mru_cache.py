56eb59ec5436c19f215bc3ca7e682d98b5b483df
Wambuuyy <wambuiprudy@gmail.com>
2024-07-25 09:14:41 +0300

    mru

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" MRUCache module
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache defines:
+      - a MRU caching system
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = next(reversed(self.cache_data))
+            print(f"DISCARD: {discard}")
+            self.cache_data.pop(discard)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
