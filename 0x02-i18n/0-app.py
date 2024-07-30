3c756e3c734ddc5efcec7b6ee0e7eb3390f4736c
Wambuuyy <wambuiprudy@gmail.com>
2024-07-30 21:03:19 +0300

    correct documentation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """first page"""
     return render_template('0-index.html')
 
 
