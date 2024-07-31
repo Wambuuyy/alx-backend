c13dcc51a66234587b117d649cb8d9787f4aa462
Wambuuyy <wambuiprudy@gmail.com>
2024-07-31 10:56:06 +0300

    documentation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,26 +3,31 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
 class Config:
-    """doc"""
+    """enough class documentation """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    """doc"""
+    """doc doc doc"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
-    """doc"""
+    """doc doc doc"""
     return render_template('3-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
