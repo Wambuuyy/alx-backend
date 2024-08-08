626f385c75b7348deeadb1f28e2ab7e164938bcd
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-08-08 07:47:48 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
-""" LRUCache module
-"""
+""" 4. LRU Caching """
+
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
-    """ LRUCache defines:
-      - a LRU caching system
-    """
+    """ LRUCache defines a Least Recently Used caching system """
 
     def __init__(self):
-        """ Initialize
-        """
+        """ Initialize """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.order = []
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ Add an item in the cache """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.cache_data.move_to_end(key)
+            self.order.remove(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            least_recent = self.order.pop(0)
+            del self.cache_data[least_recent]
+            print(f"DISCARD: {least_recent}")
 
         self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {discarded_key}")
+        self.order.append(key)
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is None or key not in self.cache_data:
-            return None
-
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
