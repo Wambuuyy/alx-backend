c5320869b153ef15cbd69e9e61b0fd9e6452c692
Wambuuyy <wambuiprudy@gmail.com>
2024-08-28 09:26:25 +0300

    test: verify main route displays the correct number of available seats

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,32 @@
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  beforeEach(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+    queue.testMode.exit();
+  });
+
+  it('should throw an error if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs({}, queue)).to.throw('Jobs is not an array');
+  });
+
+  it('should create jobs and add them to the queue', () => {
+    const jobs = [
+      { phoneNumber: '4153518780', message: 'Test message 1' },
+      { phoneNumber: '4153518781', message: 'Test message 2' },
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    const queuedJobs = queue.testMode.jobs('push_notification_code_3');
+    expect(queuedJobs.length).to.equal(2);
+  });
+});
